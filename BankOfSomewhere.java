79d1a77f09e7e25bfda926699f0e77aae6f883f6
Gabe Arroyo <gabearroyo1114@gmail.com>
2021-09-08 17:49:30 -0400

    Update BankOfSomewhere.java

--- BankOfSomewhere.java
+++ BankOfSomewhere.java
@@ -3,10 +3,10 @@ public class FirstBank {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Account tim = new Account("Tim Statler", "A00001");
+		Account tim = new Account("Gabe Arroyo", "A00001");
 		
 		
-		Account beyonce = new Account("Beyonce", "B00002");
+		Account beyonce = new Account("Bart Simpson", "B00002");
 		beyonce.showMenu();
 		
 		
